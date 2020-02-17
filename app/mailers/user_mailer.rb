3e058c1490c619e9ab5d1b57cdfa7522a213a0a0
Le Lan-Toson <clelantoson@hotmail.fr>
2020-02-17 23:42:47 +0100

    attendance email working on local

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,16 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def attendance_created_email(attendance)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @attendance = attendance
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @attendance.event.admin.email, subject: 'Un participant est inscrit à ton événement !') 
+  end
+
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  after_create :attendance_created_email
+
+  def attendance_created_email
+    UserMailer.attendance_created_email(self).deliver_now
+  end
 end
