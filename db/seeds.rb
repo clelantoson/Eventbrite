2886732679cfcd999adf2837f5abf72c0fb3ab39
Le Lan-Toson <clelantoson@hotmail.fr>
2020-02-18 13:51:25 +0100

    added navbar & devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
 u = User.create(first_name: "camille", last_name: "lelan", description: "je suis mignonne ouais ouais lol xd", email: "cam@yopmail.com", encrypted_password: "gregre")
 u2 = User.create(first_name: "yann", last_name: "petitjean", description: "looooooooongue description", email: "yann@yopmail.com", encrypted_password: "nouille")
 e = Event.create(start_date:"Mon, 17 Dec 2021 00:00:00 +0000", duration: 50, title: "La fievre du lundi soir", description: "longue longue longue  description", price: 10, location: "Paris", admin: u)
